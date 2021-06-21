8e52d1d002a128c5672a1c8224d6299a005d11e7
Jean-Noel-Esc <60701865+Jean-Noel-Esc@users.noreply.github.com>
2021-06-21 12:36:16 +0200

    update MenuItem with hooks lifecycle

--- cafe-with-a-view/cli-version/src/components/MenuItem.vue
+++ cafe-with-a-view/cli-version/src/components/MenuItem.vue
@@ -1,8 +1,30 @@
 <script>
 export default {
 	name: "MenuItem",
-	props: ["addToShoppingCart", "image", "inStock", "name", "quantity"]
+	props: ["addToShoppingCart", "image", "inStock", "name", "price", "quantity"],
+	data() {
+		return {
+			onSale: false
+		}
+	},
+	computed: {
+		generatedPrice() {
+			if (this.onSale) {
+				return (this.price * 0.9).toFixed(2)
+			} else {
+				return this.price
+			}
+		}
+	},
+	beforeMount() {
+		const today = new Date().getDate()
+		if (today % 2 === 0) {
+			this.onSale = true
+		}
+	}
 }
+
+
 </script>
 
 <template>
